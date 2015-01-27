47f005dbdc0ffe8e61cf96cbd7f1b8a185964c47
Maciej Czuchnowski <mczuchnowski@users.noreply.github.com>
2015-01-27 19:52:26 +0100

    Implement registration and authentication tests

---
+++ spec/features/user/authentication_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature "Logging in" do
+let!(:user) { create(:user) }
+
+  scenario "logs the user in and redirects to the statuses page" do
+    visit login_path
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_button 'Log in'
+
+    expect(page).to have_content('All Statuses')
+    expect(page).to have_content('Logged in successfully.')
+  end
+
+  scenario "displays email address when login failed" do
+    visit login_path
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: 'wrongpassword'
+
+    click_button 'Log in'
+
+    expect(page).to have_content('Invalid email or password.')
+    expect(page).to have_field('Email', with: user.email)
+  end
+
+end
--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -7,6 +7,21 @@ module AuthenticationHelpers
       fill_in "Password", with: user.password
       click_button 'Log in'
     end
+
+    def register_user
+      visit "/"
+      within ".navbar" do
+        expect(page).to have_content("Register")
+        click_link "Register"
+      end
+
+      fill_in "Name", with: "John"
+      fill_in "Email", with: "jasmine@mail.com"
+      fill_in "Password", with: "treebook666"
+      fill_in "Confirm password", with: "treebook666"
+
+      click_button "Register"
+    end
   end
 
 end
